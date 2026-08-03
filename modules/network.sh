f856b443decedfd31a029e9964056022e46be581
afonso0206 <afonso0206@gmail.com>
2026-08-03 12:51:31 -0300

    refactor(network): add reusable data collection API

--- modules/network.sh
+++ modules/network.sh
@@ -22,10 +22,10 @@ fi
 readonly SYSGUARDIAN_NETWORK_LOADED=1
 
 #===============================================================================
-# INTERFACE PADRÃO
+# CAMADA 1 - API DE COLETA (GETTERS)
 #===============================================================================
 
-network_default_interface() {
+network_get_default_interface() {
 
     if command_exists ip; then
         ip route 2>/dev/null | awk '/default/ {print $5; exit}'
@@ -33,192 +33,206 @@ network_default_interface() {
 
 }
 
-#===============================================================================
-# HOSTNAME
-#===============================================================================
+network_get_hostname() {
 
-network_hostname() {
-
-    printf "Hostname.............: %s\n" "$(hostname)"
+    hostname
 
 }
 
-#===============================================================================
-# FQDN
-#===============================================================================
-
-network_fqdn() {
-
-    local fqdn="Desconhecido"
+network_get_fqdn() {
 
     if command_exists hostname; then
-        fqdn="$(hostname -f 2>/dev/null || echo "Desconhecido")"
+        hostname -f 2>/dev/null || printf "Desconhecido"
+    else
+        printf "Desconhecido"
     fi
-
-    printf "FQDN.................: %s\n" "$fqdn"
 
 }
 
-#===============================================================================
-# INTERFACE PRINCIPAL
-#===============================================================================
-
-network_interface() {
+network_get_interface() {
 
     local iface
 
-    iface="$(network_default_interface)"
+    iface="$(network_get_default_interface)"
 
-    printf "Interface............: %s\n" \
-        "${iface:-Desconhecida}"
+    printf "%s" "${iface:-Desconhecida}"
 
 }
 
-#===============================================================================
-# IPv4
-#===============================================================================
-
-network_ipv4() {
-
-    local ipv4="Desconhecido"
+network_get_ipv4() {
 
     if command_exists ip; then
 
-        ipv4="$(
-            ip -4 addr show scope global |
-            awk '
-                /inet / {
-                    split($2,a,"/")
-                    print a[1]
-                    exit
-                }
-            '
-        )"
+        ip -4 addr show scope global |
+        awk '
+            /inet / {
+                split($2,a,"/")
+                print a[1]
+                exit
+            }
+        '
+
+    else
+
+        printf "Desconhecido"
 
     fi
 
-    printf "IPv4.................: %s\n" \
-        "${ipv4:-Desconhecido}"
-
 }
 
-#===============================================================================
-# IPv6
-#===============================================================================
-
-network_ipv6() {
-
-    local ipv6="Não disponível"
+network_get_ipv6() {
 
     if command_exists ip; then
 
-        ipv6="$(
-            ip -6 addr show scope global |
-            awk '/inet6 / {print $2; exit}'
-        )"
+        ip -6 addr show scope global |
+        awk '/inet6 / {print $2; exit}'
+
+    else
+
+        printf "Não disponível"
 
     fi
 
-    printf "IPv6.................: %s\n" \
-        "${ipv6:-Não disponível}"
-
 }
 
-#===============================================================================
-# GATEWAY PADRÃO
-#===============================================================================
-
-network_gateway() {
-
-    local gateway="Desconhecido"
+network_get_gateway() {
 
     if command_exists ip; then
 
-        gateway="$(
-            ip route |
-            awk '/default/ {print $3; exit}'
-        )"
+        ip route |
+        awk '/default/ {print $3; exit}'
+
+    else
+
+        printf "Desconhecido"
 
     fi
 
-    printf "Gateway..............: %s\n" \
-        "${gateway:-Desconhecido}"
-
 }
 
-#===============================================================================
-# DNS
-#===============================================================================
-
-network_dns() {
-
-    local dns="Desconhecido"
+network_get_dns() {
 
     if file_exists "/etc/resolv.conf"; then
 
-        dns="$(
-            awk '/^nameserver/ {print $2}' /etc/resolv.conf |
-            paste -sd ", " -
-        )"
+        awk '/^nameserver/ {print $2}' /etc/resolv.conf |
+        paste -sd ", " -
+
+    else
+
+        printf "Desconhecido"
 
     fi
 
-    printf "DNS..................: %s\n" \
-        "${dns:-Desconhecido}"
-
 }
 
-#===============================================================================
-# MAC ADDRESS
-#===============================================================================
-
-network_mac() {
+network_get_mac() {
 
     local iface
-    local mac="Desconhecido"
 
-    iface="$(network_default_interface)"
+    iface="$(network_get_default_interface)"
 
     if [[ -n "$iface" ]] &&
        file_exists "/sys/class/net/${iface}/address"; then
 
-        mac="$(<"/sys/class/net/${iface}/address")"
+        cat "/sys/class/net/${iface}/address"
+
+    else
+
+        printf "Desconhecido"
 
     fi
 
-    printf "MAC Address..........: %s\n" "$mac"
-
 }
 
-#===============================================================================
-# STATUS DA INTERFACE
-#===============================================================================
-
-network_state() {
+network_get_state() {
 
     local iface
-    local state="Desconhecido"
 
-    iface="$(network_default_interface)"
+    iface="$(network_get_default_interface)"
 
     if [[ -n "$iface" ]] &&
        file_exists "/sys/class/net/${iface}/operstate"; then
 
-        state="$(<"/sys/class/net/${iface}/operstate")"
+        cat "/sys/class/net/${iface}/operstate"
+
+    else
+
+        printf "Desconhecido"
 
     fi
-
-    printf "Estado...............: %s\n" "$state"
 
 }
 
 #===============================================================================
-# EXECUÇÃO
+# CAMADA 2 - APRESENTAÇÃO
 #===============================================================================
 
-#
-# Função pública do módulo.
-#
+network_hostname() {
+
+    printf "Hostname.............: %s\n" \
+        "$(network_get_hostname)"
+
+}
+
+network_fqdn() {
+
+    printf "FQDN.................: %s\n" \
+        "$(network_get_fqdn)"
+
+}
+
+network_interface() {
+
+    printf "Interface............: %s\n" \
+        "$(network_get_interface)"
+
+}
+
+network_ipv4() {
+
+    printf "IPv4.................: %s\n" \
+        "$(network_get_ipv4)"
+
+}
+
+network_ipv6() {
+
+    printf "IPv6.................: %s\n" \
+        "$(network_get_ipv6)"
+
+}
+
+network_gateway() {
+
+    printf "Gateway..............: %s\n" \
+        "$(network_get_gateway)"
+
+}
+
+network_dns() {
+
+    printf "DNS..................: %s\n" \
+        "$(network_get_dns)"
+
+}
+
+network_mac() {
+
+    printf "MAC Address..........: %s\n" \
+        "$(network_get_mac)"
+
+}
+
+network_state() {
+
+    printf "Estado...............: %s\n" \
+        "$(network_get_state)"
+
+}
+
+#===============================================================================
+# CAMADA 3 - EXECUÇÃO
+#===============================================================================
 
 network_run() {
 
